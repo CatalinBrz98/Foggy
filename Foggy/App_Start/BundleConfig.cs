7529efd6d56b4e7826c2908a7a9527a9d02669d0
Breaz Catalin <catalin.brz98@yahoo.com>
2021-01-13 21:05:53 +0200

    Frontend Change

--- Foggy/App_Start/BundleConfig.cs
+++ Foggy/App_Start/BundleConfig.cs
@@ -20,10 +20,12 @@ namespace Foggy
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-navbar.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-darkly.css",
+                      "~/Content/bootstrap-navbar.css",
                       "~/Content/site.css"));
         }
     }
